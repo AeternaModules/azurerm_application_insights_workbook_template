e7e48691625b5df384516cceaa5e77eb4d8b424c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "application_insights_workbook_templates" {
-  description = "All application_insights_workbook_template resources"
-  value       = azurerm_application_insights_workbook_template.application_insights_workbook_templates
-}
 output "application_insights_workbook_templates_author" {
-  description = "List of author values across all application_insights_workbook_templates"
-  value       = [for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : v.author]
+  description = "Map of author values across all application_insights_workbook_templates, keyed the same as var.application_insights_workbook_templates"
+  value       = { for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : k => v.author }
 }
 output "application_insights_workbook_templates_galleries" {
-  description = "List of galleries values across all application_insights_workbook_templates"
-  value       = [for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : v.galleries]
+  description = "Map of galleries values across all application_insights_workbook_templates, keyed the same as var.application_insights_workbook_templates"
+  value       = { for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : k => v.galleries }
 }
 output "application_insights_workbook_templates_localized" {
-  description = "List of localized values across all application_insights_workbook_templates"
-  value       = [for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : v.localized]
+  description = "Map of localized values across all application_insights_workbook_templates, keyed the same as var.application_insights_workbook_templates"
+  value       = { for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : k => v.localized }
 }
 output "application_insights_workbook_templates_location" {
-  description = "List of location values across all application_insights_workbook_templates"
-  value       = [for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : v.location]
+  description = "Map of location values across all application_insights_workbook_templates, keyed the same as var.application_insights_workbook_templates"
+  value       = { for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : k => v.location }
 }
 output "application_insights_workbook_templates_name" {
-  description = "List of name values across all application_insights_workbook_templates"
-  value       = [for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : v.name]
+  description = "Map of name values across all application_insights_workbook_templates, keyed the same as var.application_insights_workbook_templates"
+  value       = { for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : k => v.name }
 }
 output "application_insights_workbook_templates_priority" {
-  description = "List of priority values across all application_insights_workbook_templates"
-  value       = [for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : v.priority]
+  description = "Map of priority values across all application_insights_workbook_templates, keyed the same as var.application_insights_workbook_templates"
+  value       = { for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : k => v.priority }
 }
 output "application_insights_workbook_templates_resource_group_name" {
-  description = "List of resource_group_name values across all application_insights_workbook_templates"
-  value       = [for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : v.resource_group_name]
+  description = "Map of resource_group_name values across all application_insights_workbook_templates, keyed the same as var.application_insights_workbook_templates"
+  value       = { for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : k => v.resource_group_name }
 }
 output "application_insights_workbook_templates_tags" {
-  description = "List of tags values across all application_insights_workbook_templates"
-  value       = [for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : v.tags]
+  description = "Map of tags values across all application_insights_workbook_templates, keyed the same as var.application_insights_workbook_templates"
+  value       = { for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : k => v.tags }
 }
 output "application_insights_workbook_templates_template_data" {
-  description = "List of template_data values across all application_insights_workbook_templates"
-  value       = [for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : v.template_data]
+  description = "Map of template_data values across all application_insights_workbook_templates, keyed the same as var.application_insights_workbook_templates"
+  value       = { for k, v in azurerm_application_insights_workbook_template.application_insights_workbook_templates : k => v.template_data }
 }
 
